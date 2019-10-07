847ee380463727f9ae4f742514245d8445bcd0e0
Chigozie Orunta <chigozieorunta@yahoo.com>
2019-10-07 14:51:51 +0100

    Update wp-popups.php

--- wp-popups.php
+++ wp-popups.php
@@ -27,7 +27,7 @@ class wpPopups {
 	 */
     public function __construct() {
         add_action('admin_menu', array(get_called_class(), 'registerMenu'));
-        self::registerCustomPostTypes();
+        self::registerPopupType();
     }
 
     /**
@@ -36,9 +36,8 @@ class wpPopups {
 	 * @since   06/10/2019
 	 * @return  void
 	 */
-	private static function registerCustomPostTypes() {	
-        	add_action('init', array(get_called_class(), 'postTypeInit'));	
-        	add_action('init', array(get_called_class(), 'setPostTypes'));
+	private static function registerPopupType() {	
+        	add_action('init', array(get_called_class(), 'postTypeInit'));
 	}
 
     /**
@@ -64,7 +63,7 @@ class wpPopups {
 	 * @since  1.0.0
 	 */
     public static function registerHTML() {
-        //require_once('wp-posttypes-html.php');
+        //require_once('wp-popups-html.php');
     }
 
     /**
@@ -87,22 +86,22 @@ class wpPopups {
 	 */
     public static function postTypeInit() {
         $labels = array(
-		'name'                => _x('Types', 'Post Type General Name'),
-		'singular_name'       => _x('Type', 'Post Type Singular Name'),
-		'menu_name'           => __('Types'),
+		'name'                => _x('Popup', 'Post Type General Name'),
+		'singular_name'       => _x('Popup', 'Post Type Singular Name'),
+		'menu_name'           => __('Popups'),
 		'parent_item_colon'   => __('Parent Type'),
-		'all_items'           => __('All Types'),
-		'view_item'           => __('View Type'),
-		'add_new_item'        => __('Add New Type'),
+		'all_items'           => __('All Popups'),
+		'view_item'           => __('View Popup'),
+		'add_new_item'        => __('Add New Popup'),
 		'add_new'             => __('Add New'),
-		'edit_item'           => __('Edit Type'),
-		'update_item'         => __('Update Type'),
-		'search_items'        => __('Search Type'),
+		'edit_item'           => __('Edit Popup'),
+		'update_item'         => __('Update Popup'),
+		'search_items'        => __('Search Popup'),
 		'not_found'           => __('Not Found'),
 		'not_found_in_trash'  => __('Not found in Trash')
 	);
 	$args = array(
-		'label'               => __('Type'),
+		'label'               => __('Popup'),
 		'description'         => __('News & Reviews'),
 		'labels'              => $labels,
 		'supports'            => array( 'title', 'editor', 'excerpt', 'author', 'thumbnail', 'comments', 'revisions', 'custom-fields' ),
@@ -120,66 +119,8 @@ class wpPopups {
 		'publicly_queryable'  => true,
 		'capability_type'     => 'page'
 	);
-	register_post_type('type', $args);
+	register_post_type('popup', $args);
     }
-
-    /**
-	 * Set Post Types Method
-	 *
-     * @access public 
-	 * @since  1.0.0
-	 */
-    public static function setPostTypes() {
-	    global $post;
-	    $args = array(
-		'post_type'         => 'type',
-		'numberposts'       => -1,
-		'posts_per_page'    => -1,
-		'orderby'           => 'date',
-		'order'             => 'ASC'
-	    );
-	    $posts = get_posts($args);
-	    foreach($posts as $post) {
-		setup_postdata($post); $title = get_the_title();
-		$labels = array(
-			'name'                => _x($title.'s', 'Post Type General Name'),
-			'singular_name'       => _x($title, 'Post Type Singular Name'),
-			'menu_name'           => __($title.'s'),
-			'parent_item_colon'   => __('Parent '.$title),
-			'all_items'           => __('All '.$title.'s'),
-			'view_item'           => __('View '.$title),
-			'add_new_item'        => __('Add New '.$title),
-			'add_new'             => __('Add New'),
-			'edit_item'           => __('Edit '.$title),
-			'update_item'         => __('Update '.$title),
-			'search_items'        => __('Search '.$title),
-			'not_found'           => __('Not Found'),
-			'not_found_in_trash'  => __('Not found in Trash'),
-		);
-
-		$args = array(
-			'label'               => __($title),
-			'description'         => __( 'News and Reviews'),
-			'labels'              => $labels,
-			'supports'            => array( 'title', 'editor', 'excerpt', 'author', 'thumbnail', 'comments', 'revisions', 'custom-fields' ),
-			'hierarchical'        => true,
-			'public'              => true,
-			'show_ui'             => true,
-			'show_in_menu'        => true,
-			'show_in_nav_menus'   => true,
-			'show_in_admin_bar'   => true,
-			'menu_position'       => 30,
-		    'menu_icon'           => 'dashicons-admin-tools',
-			'can_export'          => true,
-			'has_archive'         => true,
-			'exclude_from_search' => false,
-			'publicly_queryable'  => true,
-			'capability_type'     => 'page'
-		);
-		register_post_type(strtolower($title), $args );
-	    }
-	    wp_reset_postdata();
-     }
 	
 }
 
